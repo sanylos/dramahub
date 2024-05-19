ee309d34d0e54dac5617b703d1ab14a4872eccf8
sanyl <homolkadam@seznam.cz>
2024-05-19 19:20:51 +0200

    Added responsivity

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className="p-4 sm:ml-64">
+          {children}
+        </div>
       </body>
     </html>
   );
