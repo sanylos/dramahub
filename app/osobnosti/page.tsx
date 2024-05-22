677f8f19cd5f5eec04872f88d8f024cd6930684d
sanyl <homolkadam@seznam.cz>
2024-05-22 20:14:18 +0200

    Added celebrity rendering

--- app/osobnosti/page.tsx
+++ app/osobnosti/page.tsx
@@ -2,9 +2,10 @@
 import ProfileCard from "@/components/ProfileCard"
 import { createClient } from '@/utils/supabase/client'
 import { useEffect, useState } from "react";
+import { Celebrity } from "../types/celebrity.types";
 const OsobnostiPage = () => {
     const supabase = createClient()
-    const [celebrities, setCelebrities] = useState([]);
+    const [celebrities, setCelebrities] = useState<Celebrity[]>([]);
     const fetchCelebrities = async () => {
         const { data } = await supabase
             .from('celebrities').select()
@@ -17,7 +18,10 @@ const OsobnostiPage = () => {
         <div>
             <h1>Osobnosti</h1>
             {JSON.stringify(celebrities)}
-            <ProfileCard />
+            <div className="flex">
+                {celebrities && celebrities.map(celebrity => <ProfileCard key={celebrity.id} celebrity={celebrity} />)}
+            </div>
+
         </div>
     )
 }
