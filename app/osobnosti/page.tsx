9c21638f78c50a5e2f9fff4634a3f5df25fe28dd
sanyl <homolkadam@seznam.cz>
2024-05-22 20:01:31 +0200

    Added celebrities fetch

--- app/osobnosti/page.tsx
+++ app/osobnosti/page.tsx
@@ -1,8 +1,22 @@
+"use client"
 import ProfileCard from "@/components/ProfileCard"
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from "react";
 const OsobnostiPage = () => {
+    const supabase = createClient()
+    const [celebrities, setCelebrities] = useState([]);
+    const fetchCelebrities = async () => {
+        const { data } = await supabase
+            .from('celebrities').select()
+        setCelebrities(data);
+    }
+    useEffect(() => {
+        fetchCelebrities();
+    })
     return (
         <div>
             <h1>Osobnosti</h1>
+            {JSON.stringify(celebrities)}
             <ProfileCard />
         </div>
     )
