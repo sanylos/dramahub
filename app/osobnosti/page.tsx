5caa9081ae99197166fa5a6c191766cd7d3443be
sanyl <homolkadam@seznam.cz>
2024-05-23 20:02:15 +0200

    Added env variable

--- app/osobnosti/page.tsx
+++ app/osobnosti/page.tsx
@@ -4,7 +4,7 @@ import { Celebrity } from "../types/celebrity.types";
 import { createClient } from "@supabase/supabase-js";
 
 export const fetchCelebrities = async () => {
-    const res = await fetch('http://localhost:3000/api/celebrities');
+    const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/celebrities');
     const data = res.json();
     return data;
 }
