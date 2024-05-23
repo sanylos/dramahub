2deb764f4945968747d2204502c23fd73f72414f
sanyl <homolkadam@seznam.cz>
2024-05-23 19:57:23 +0200

    Added celebrity endpoint

---
+++ app/api/celebrities/[nickname]/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { nickname: string } }) {
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    )
+    const { data } = await supabase
+        .from('celebrities')
+        .select('*')
+        .ilike('nickname', params.nickname)
+        .single();
+    if (data) return NextResponse.json(Object.values(data));
+    return null;
+}
