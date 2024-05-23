99eab5fcfdfad5f939ec5255c06c1c3bfd8af86c
sanyl <homolkadam@seznam.cz>
2024-05-23 19:52:54 +0200

    Added api call

--- app/osobnosti/page.tsx
+++ app/osobnosti/page.tsx
@@ -4,13 +4,9 @@ import { Celebrity } from "../types/celebrity.types";
 import { createClient } from "@supabase/supabase-js";
 
 export const fetchCelebrities = async () => {
-    const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    )
-    const { data } = await supabase.from('celebrities').select();
-    if (data) return data;
-    return null;
+    const res = await fetch('http://localhost:3000/api/celebrities');
+    const data = res.json();
+    return data;
 }
 export const revalidate = 10;
 const OsobnostiPage = async () => {
