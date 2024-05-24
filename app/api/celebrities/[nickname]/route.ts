8f65bfdf0d304f9dbcec5095f1add5b6e099eff4
sanyl <homolkadam@seznam.cz>
2024-05-24 19:20:25 +0200

    Used util instead of absolute definition

--- app/api/celebrities/[nickname]/route.ts
+++ app/api/celebrities/[nickname]/route.ts
@@ -1,11 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "@/utils/supabase/supabaseClient";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, { params }: { params: { nickname: string } }) {
-    const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    )
     const { data } = await supabase
         .from('celebrities')
         .select('*')
