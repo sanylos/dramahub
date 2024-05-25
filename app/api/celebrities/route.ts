db0df777ab6cadfade115392d434c0c368ba9955
sanyl <homolkadam@seznam.cz>
2024-05-25 18:53:37 +0200

    Imported supabase utility

--- app/api/celebrities/route.ts
+++ app/api/celebrities/route.ts
@@ -1,15 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
-
+import { supabase } from "@/utils/supabase/supabaseClient";
 
 export async function GET(reques: Request) {
-    const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    )
     const { data } = await supabase
         .from('celebrities')
-        .select()
+        .select('*')
     if (data) return NextResponse.json(Object.values(data));
     return null;
 }
