602fbf35b6e712f5bb4ab8f5f2dfbb82e5ff7247
sanyl <homolkadam@seznam.cz>
2024-05-25 19:08:07 +0200

    Added profile link

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import { Celebrity } from "@/app/types/celebrity.types"
+import Link from "next/link";
 
 export const fetchProfilePictureUrl = async (nickname: string) => {
     const res = await fetch(process.env.NEXT_PUBLIC_URL + '/api/celebrities/' + nickname + '/youtube/profile-picture');
@@ -39,7 +40,7 @@ const ProfileCard = async ({ celebrity }: { celebrity: Celebrity }) => {
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{celebrity.nickname || celebrity.real_name}</h5>
                 <span className="text-sm text-gray-500 dark:text-gray-400">{celebrity.real_name}</span>
                 <div className="flex mt-4 md:mt-6">
-                    <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Zobrazit</a>
+                    <Link href={"/osobnosti/" + celebrity.nickname} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Zobrazit</Link>
                     <a href="#" className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</a>
                 </div>
             </div>
