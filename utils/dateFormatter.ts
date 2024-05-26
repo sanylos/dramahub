d2717b8cc3b5435043fe381b46f1ceb212f703e8
sanyl <homolkadam@seznam.cz>
2024-05-26 19:54:24 +0200

    Added number flooring

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -1,8 +1,8 @@
 export const formatDays = (days: number) => {
     if (days <= 31) {
-        if (days == 1) return days + ' den';
-        if (days < 4) return days + ' dny';
-        return days + ' dní';
+        if (days == 1) return Math.floor(days) + ' den';
+        if (days < 4) return Math.floor(days) + ' dny';
+        return Math.floor(days) + ' dní';
     }
     if (days <= 365) return Math.floor(days / 30.4166667) + ' měsíců';
     if (days / 365 <= 1) return '1 rok';
