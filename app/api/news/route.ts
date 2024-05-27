e8d8d5bf9dd59b870f6abcabcd0b5ebaeda41758
sanyl <homolkadam@seznam.cz>
2024-05-27 20:04:38 +0200

    Created news endpoint

---
+++ app/api/news/route.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
+import { supabase } from "@/utils/supabase/supabaseClient";
+
+export async function GET(reques: Request) {
+    const { data } = await supabase
+        .from('news')
+        .select('*')
+    if (data) return NextResponse.json(Object.values(data));
+    return null;
+}
