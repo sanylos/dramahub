0e00850a9f7af6986fc1ebefdf17d34edf1fbbf9
sanyl <homolkadam@seznam.cz>
2024-06-03 20:02:18 +0200

    Updated API response

--- app/api/celebrities/fire/route.ts
+++ app/api/celebrities/fire/route.ts
@@ -1,13 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 import { supabase } from "@/utils/supabase/supabaseClient";
+import { Celebrity } from "@/app/types/celebrity.types";
 
 export async function GET(reques: Request) {
     const { data } = await supabase
         .from('news')
         .select('celebrities_news(celebrities(*))')
         .order('created_at', { ascending: false })
-        .limit(4)
-    if (data) return NextResponse.json(Object.values(data.map(item => { return item.celebrities_news[0].celebrities })));
+        .limit(4);
+    const celebrities: Celebrity[] | undefined = data?.map(item => { return item.celebrities_news[0]?.celebrities }).flat();
+    if (data) return NextResponse.json(Array.from(new Map(celebrities?.map(celebrity => [celebrity.id, celebrity])).values()));
     return null;
 }
